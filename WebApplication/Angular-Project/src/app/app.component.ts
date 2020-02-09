763442bd6e6bc87f167a167319955776bcae83a3
Anjali Shehani <anjali.sw66@gmail.com>
2020-02-09 21:40:43 +0530

    Image Slider created

--- WebApplication/Angular-Project/src/app/app.component.ts
+++ WebApplication/Angular-Project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AgroBuddy';
+  images = ['assets/images/1.jpg', 'assets/images/2.jpg', 'assets/images/3.jpg'];
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 5000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+    config.wrap = true;
+  }
 }
--- WebApplication/Angular-Project/src/app/app.module.ts
+++ WebApplication/Angular-Project/src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule , routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-//import { HomeComponent } from './home/home.component';
+/*import { HomeComponent } from './home/home.component';*/
 
 @NgModule({
   declarations: [
@@ -13,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
