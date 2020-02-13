40acb226d2028c8e237be81e79feb4b74e4ab710
sachintha97 <ssamarasiri12@gmail.com>
2020-02-13 15:44:18 +0530

    change home

--- WebApplication/Angular-Project/src/app/home/home.component.ts
+++ WebApplication/Angular-Project/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  images = ['assets/images/1.jpg', 'assets/images/2.jpg', 'assets/images/3.jpg'];
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 5000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+    config.wrap = true;
+  }
 
-  constructor() { }
+  
 
   ngOnInit() {
   }
