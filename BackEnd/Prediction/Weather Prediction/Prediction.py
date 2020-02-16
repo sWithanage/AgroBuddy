8d7260ba38209423b06c3ce62c1872a0716aa944
Sasanka Withanage <awsnbwithanage@gmail.com>
2020-02-16 23:38:43 +0530

    Univariate Time Prediction of weather

--- BackEnd/Prediction/Weather Prediction/Prediction.py
+++ BackEnd/Prediction/Weather Prediction/Prediction.py
@@ -43,5 +43,47 @@ df.head()
 # Check weather data correctly imported or not.
 print(df.head())
 
-# This is changed
+# One variable is varying overtime.
+def univariate_data(dataset, start_index, end_index, history_size, target_size):
 
+    data = []
+    labels = []
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+        indices = range(i-history_size, i)
+        # Reshape data from (history_size,) to (history_size, 1)
+        data.append(np.reshape(dataset[indices], (history_size, 1)))
+        labels.append(dataset[i+target_size])
+        return np.array(data), np.array(labels)
+
+TRAIN_SPLIT = 300000
+
+tf.random.set_seed(13)
+
+uni_data = df['T (degC)']
+uni_data.index = df['Date Time']
+uni_data.head()
+
+
+uni_data.plot(subplots=True)
+
+uni_data = uni_data.values
+
+uni_train_mean = uni_data[:TRAIN_SPLIT].mean()
+uni_train_std = uni_data[:TRAIN_SPLIT].std()
+
+uni_data = (uni_data-uni_train_mean)/uni_train_std
+
+univariate_past_history = 20
+univariate_future_target = 0
+
+x_train_uni, y_train_uni = univariate_data(uni_data, 0, TRAIN_SPLIT,univariate_past_history,univariate_future_target)
+x_val_uni, y_val_uni = univariate_data(uni_data, TRAIN_SPLIT, None,univariate_past_history,univariate_future_target)
+
+print ('Single window of past history')
+print (x_train_uni[0])
+print ('\n Target temperature to predict')
+print (y_train_uni[0])
