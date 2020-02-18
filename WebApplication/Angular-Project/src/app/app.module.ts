b583d3ec35d1cbc429028f2b194ae81c3273eb45
sachintha97 <ssamarasiri12@gmail.com>
2020-02-18 20:00:23 +0530

    add : angular material nav bar

--- WebApplication/Angular-Project/src/app/app.module.ts
+++ WebApplication/Angular-Project/src/app/app.module.ts
@@ -4,6 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule , routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule ,MatButtonModule ,MatSidenavModule,MatIconModule ,MatListModule } from '@angular/material';
+//import { MatButtonModule } from '@angular/material/button';
+//import { MatSidenavModule } from '@angular/material/sidenav';
+//import { MatIconModule } from '@angular/material/icon';
+//import { MatListModule } from '@angular/material/list';
 
 /*import { HomeComponent } from './home/home.component';*/
 
@@ -11,11 +19,19 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
   declarations: [
     AppComponent,
     routingComponents,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
 
   ],
   providers: [],
