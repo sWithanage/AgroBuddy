bc0bdaa0450894f5e35c6017b49fe72d7634fd9f
Thimudu Pehesara <thimudu@gmail.com>
2020-02-19 14:19:48 +0530

    register component updated.

--- WebApplication/Angular-Project/src/app/app-routing.module.ts
+++ WebApplication/Angular-Project/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {RegisterFormComponent} from './register-form/register-form.component';
 
 
 const routes: Routes = [
-  {path:'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'register-form', component: RegisterFormComponent }
+
 ];
 
 @NgModule({
@@ -13,4 +16,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [HomeComponent]
+export const routingComponents = [HomeComponent, RegisterFormComponent];
