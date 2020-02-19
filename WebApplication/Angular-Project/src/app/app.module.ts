eb6e101525ff9792ee0dc4e801dd6b3461979839
Thimudu Pehesara <thimudu@gmail.com>
2020-02-19 13:57:33 +0530

    Register_component created

--- WebApplication/Angular-Project/src/app/app.module.ts
+++ WebApplication/Angular-Project/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule ,MatButtonModule ,MatSidenavModule,MatIconModule ,MatListModule } from '@angular/material';
+import { RegisterFormComponent } from './register-form/register-form.component';
 //import { MatButtonModule } from '@angular/material/button';
 //import { MatSidenavModule } from '@angular/material/sidenav';
 //import { MatIconModule } from '@angular/material/icon';
@@ -20,6 +21,7 @@ import { MatToolbarModule ,MatButtonModule ,MatSidenavModule,MatIconModule ,MatL
     AppComponent,
     routingComponents,
     NavBarComponent,
+    RegisterFormComponent,
   ],
   imports: [
     BrowserModule,
