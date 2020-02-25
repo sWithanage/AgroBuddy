b2b089ee1ef2e560b73990aaa6eb7a7ce92bdbb9
Thimudu Pehesara <thimudu@gmail.com>
2020-02-25 08:52:06 +0530

    update register form

--- WebApplication/Angular-Project/src/app/app.module.ts
+++ WebApplication/Angular-Project/src/app/app.module.ts
@@ -10,10 +10,11 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule ,MatButtonModule ,MatSidenavModule,MatIconModule ,MatListModule } from '@angular/material';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { RegisterFormOneComponent } from './register-form-one/register-form-one.component';
-//import { MatButtonModule } from '@angular/material/button';
-//import { MatSidenavModule } from '@angular/material/sidenav';
-//import { MatIconModule } from '@angular/material/icon';
-//import { MatListModule } from '@angular/material/list';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatListModule } from '@angular/material/list';
+
 
 /*import { HomeComponent } from './home/home.component';*/
 
