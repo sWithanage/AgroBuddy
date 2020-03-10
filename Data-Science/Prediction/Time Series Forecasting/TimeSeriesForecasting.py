83c35c27cbab33ddccd7c759f58c0a97c8efadbb
Sasanka Withanage <awsnbwithanage@gmail.com>
2020-03-10 10:53:38 +0530

    Code Fixed And ReOrganized.

--- Data-Science/Prediction/Time Series Forecasting/TimeSeriesForecasting.py
+++ Data-Science/Prediction/Time Series Forecasting/TimeSeriesForecasting.py
@@ -1,15 +1,24 @@
+# https://machinelearningmastery.com/arima-for-time-series-forecasting-with-python/
 from pandas import read_csv
 from pandas import datetime
 from matplotlib import pyplot
 from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
-
+from pandas.plotting import autocorrelation_plot
 
 def parser(x):
     return datetime.strptime('190' + x, '%Y-%m')
 
 
-series = read_csv('/shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+series = read_csv('shampoo.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+
+print(series.head())
+series.plot()
+pyplot.show()
+
+autocorrelation_plot(series)
+pyplot.show()
+
 X = series.values
 size = int(len(X) * 0.66)
 train, test = X[0:size], X[size:len(X)]
