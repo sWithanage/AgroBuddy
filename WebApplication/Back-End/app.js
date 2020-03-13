36bb416f0e92f82bfc8d8d591795b2374cced93c
sachintha97 <ssamarasiri12@gmail.com>
2020-03-13 11:44:28 +0530

    add: back-end project

---
+++ WebApplication/Back-End/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+const app = express();
+
