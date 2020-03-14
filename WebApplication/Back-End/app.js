35707d03b5a85e49567aaa16f9f4dcb4d39e7a27
SaviniAthaudage <savini.athaudage@gmail.com>
2020-03-14 10:32:41 +0530

    add: database connection to backend

--- WebApplication/Back-End/app.js
+++ WebApplication/Back-End/app.js
@@ -3,5 +3,28 @@ const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
+
 const app = express();
 
+//creating connection configuration
+var db = mysql.createConnection({
+    host : 'sql172.main-hosting.eu',
+    user : 'u499244451_agrobackEnd',
+    password : 'agrobackEnd',
+    database : 'u499244451_agrobackEnd'
+});
+
+//connect
+db.connect((err) => {
+    if(err){
+        console.log('Database connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+    }
+    else{
+        console.log('Database connection succeeded.');
+    }
+});
+
+//starting the server
+app.listen('3000', () => {
+    console.log('Server starting on port 3000');
+});
