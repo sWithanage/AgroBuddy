e621c52729bddb161e1a8fac7f61f8603def3c84
Thimudu Pehesara <thimudu@gmail.com>
2020-03-16 21:59:45 +0530

    update login and signup pages.

--- WebApplication/Angular/src/app/login/login.component.ts
+++ WebApplication/Angular/src/app/login/login.component.ts
@@ -1,17 +1,54 @@
 import { Component } from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {AuthenticationService} from '../authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
-  constructor() { }
+  Customer: any[];
+  constructor(private connectionService: AuthenticationService) { }
 
   loginform = true;
   recoverform = false;
+  username: any;
+  password: any;
+  public unameEmpty;
+  public passwordEmpty;
 
+  validator(value: any) {
+    // tslint:disable-next-line:triple-equals
+    if (String(value).length == 0) {
+      return false;
+    }
+
+    // tslint:disable-next-line:triple-equals
+    if (String(value) == 'undefined') {
+      return false;
+    }
+    return true;
+  }
+  sendCustomerLoginDetails(value: any) {
+    this.connectionService.addcustomer(value).subscribe(data => {
+      this.Customer = data;
+    });
+  }
+  submit(value: any) {
+    if (!this.validator(this.username)) {
+      this.unameEmpty = true;
+    } else if (!this.validator(this.password)) {
+      this.passwordEmpty = true;
+    } else {
+      console.log(this.username, this.password);
+      this.connectionService.addcustomerLogin(value).subscribe(
+        data => console.log(data)
+      );
+    }
+  }
   showRecoverForm() {
     this.loginform = !this.loginform;
     this.recoverform = !this.recoverform;
   }
+
 }
--- WebApplication/Angular/src/app/signup/signup.component.ts
+++ WebApplication/Angular/src/app/signup/signup.component.ts
@@ -1,19 +1,50 @@
 import { Component } from '@angular/core';
 import {Title} from '@angular/platform-browser';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html'
 })
 export class SignupComponent {
-  constructor(private titleService: Title) {
-    this.titleService.setTitle( 'DashBoard' );
+  Customer: any[];
+
+  constructor(private connectionService: AuthenticationService) {
+
   }
 
   signUpPart1 = true;
   signUpPart2 = false;
   signUpPart3 = false;
   signUpPart4 = false;
+  name: any;
+  lname: any;
+  dob: any;
+  nic: any;
+  email: any;
+  uname: any;
+  password: any;
+  confirm: any;
+  no: any;
+  st1: any;
+  st2: any;
+  city: any;
+  telno: any;
+  phoneno: any;
+  public errormassage;
+  public nameEmpty;
+  public lnameEmpty;
+  public dobEmpty;
+  public nicEmpty;
+  public emailEmpty;
+  public unameEmpty;
+  public passwordEmpty;
+  public confirmEmpty;
+  public doesnotsuitEmpty;
+  public NoEmpty;
+  public streetEmpty;
+  public cityEmpty;
+  public numberEmpty;
 
   goToPart1() {
     this.signUpPart1 = true;
@@ -21,22 +52,113 @@ export class SignupComponent {
     this.signUpPart3 = false;
     this.signUpPart4 = false;
   }
+
   goToPart2() {
-    this.signUpPart1 = false;
-    this.signUpPart2 = true;
-    this.signUpPart3 = false;
-    this.signUpPart4 = false;
+    console.log(this.name, this.lname, this.email, this.dob, this.nic, String(this.name).length);
+    if (!this.validator(this.name)) {
+        this.nameEmpty = true;
+    } else if (!this.validator(this.lname)) {
+        this.lnameEmpty = true;
+    } else if (!this.validator(this.email) && !this.emailValidator(this.email)) {
+        this.emailEmpty = true;
+    } else if (!this.validator(this.dob)) {
+        this.errormassage = 'Date of birth is required!';
+        this.dobEmpty = true;
+    } else if (!this.validator(this.nic)) {
+        this.errormassage = 'NIC number is required!';
+        this.nicEmpty = true;
+    } else {
+      this.signUpPart1 = false;
+      this.signUpPart2 = true;
+      this.signUpPart3 = false;
+      this.signUpPart4 = false;
+    }
   }
+
   goToPart3() {
-    this.signUpPart1 = false;
-    this.signUpPart2 = false;
-    this.signUpPart3 = true;
-    this.signUpPart4 = false;
+    if (!this.validator(this.uname)) {
+      this.errormassage = 'User Name is required!';
+      this.unameEmpty = true;
+    } else if (!this.validator(this.password) && !this.passwordValidator(this.password)) {
+      this.errormassage = 'Password is required!';
+      this.passwordEmpty = true;
+    } else if (!this.validator(this.confirm)) {
+      this.errormassage = 'Confirm your password is required!';
+      this.confirmEmpty = true;
+      // tslint:disable-next-line:triple-equals
+    } else if (this.password != this.confirm) {
+      this.errormassage = 'Invalid password..Try again!';
+      this.doesnotsuitEmpty = true;
+    } else {
+      this.signUpPart1 = false;
+      this.signUpPart2 = false;
+      this.signUpPart3 = true;
+      this.signUpPart4 = false;
+    }
   }
+
   goToPart4() {
-    this.signUpPart1 = false;
-    this.signUpPart2 = false;
-    this.signUpPart3 = false;
-    this.signUpPart4 = true;
+    if (!this.validator(this.no)) {
+      this.errormassage = 'No is required!';
+      this.NoEmpty = true;
+    } else if (!this.validator(this.st1)) {
+      this.errormassage = 'street1 is required!';
+      this.streetEmpty = true;
+    } else if (!this.validator(this.city)) {
+      this.errormassage = 'City is required!';
+      this.cityEmpty = true;
+    } else {
+      this.signUpPart1 = false;
+      this.signUpPart2 = false;
+      this.signUpPart3 = false;
+      this.signUpPart4 = true;
+    }
   }
-}
+
+  sendCustomerDetails(value: any) {
+    this.connectionService.addcustomer(value).subscribe(data => {
+      this.Customer = data;
+    });
+  }
+  onSubmit(value: any) {
+    console.log(this.phoneno, this.telno);
+    // tslint:disable-next-line:triple-equals
+    if (this.telno || this.phoneno == 0) {
+      this.errormassage = 'You should enter telephone number or phone number!';
+      this.numberEmpty = true;
+    } else {
+      this.connectionService.addcustomer(value).subscribe(
+        data => console.log(data)
+      );
+    }
+  }
+
+  validator(value: any) {
+    // tslint:disable-next-line:triple-equals
+    if (String(value).length == 0) {
+      return false;
+    }
+    // tslint:disable-next-line:triple-equals
+    if (String(value) == 'undefined') {
+      return false;
+    }
+    return true;
+    }
+    emailValidator(value: any) {
+      // tslint:disable-next-line:triple-equals
+      if (this.email != /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i) {
+        return false;
+      }
+    }
+    passwordValidator(value: any) {
+      if (String(this.password).length < 8) {
+        return false;
+      }
+    }
+    phonenoValidator(value: any) {
+      // tslint:disable-next-line:triple-equals
+      if (String(this.phoneno).length != 10 && String(this.telno).length != 10) {
+        return false;
+      }
+    }
+  }
