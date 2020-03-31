666673a11bffd2a587993feb11e3bcdcc8c0cd98
sachintha97 <ssamarasiri12@gmail.com>
2020-03-31 12:42:26 +0530

    update : back-end app.js, user.js and accuracy.js

--- WebApplication/Back-End/app/app.js
+++ WebApplication/Back-End/app/app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const path = require("path");
 const cron = require("node-cron");
+const mysqlConnection = require("./connection");
+const axios = require("axios");
 
 //const mysqlConnection = require("./connection");
 const PORT = 8080;
@@ -25,9 +27,41 @@ app.use(predictions);
 app.use(accuracy);
 
 // schedule tasks to be run on the server
-cron.schedule("* * * * *", function() {
+cron.schedule("* 12 * * *", async (req, res) => {
   // data retreive from api endpoints 3
   // save those data into DB
+  try {
+    const arimaTempAccuracy = await axios.get(
+      "https://agrobuddy-prediction.appspot.com/accuracy?model=arima&type=temp"
+    );
+    const arimaRaiFallAccuracy = await axios.get(
+      "https://agrobuddy-prediction.appspot.com/accuracy?model=arima&type=rainfall"
+    );
+    const arimaMarketPriceAccuracy = await axios.get(
+      "https://agrobuddy-prediction.appspot.com/accuracy?model=arima&type=market"
+    );
+
+    mysqlConnection.query(
+      "UPDATE marketprice_prediction SET model_accuracy = ? WHERE model_id = 1",
+      [arimaMarketPriceAccuracy.data],
+      () => {}
+    );
+
+    mysqlConnection.query(
+      "UPDATE temperature_prediction SET model_accuracy = ? WHERE model_id = 1",
+      [arimaTempAccuracy.data],
+      () => {}
+    );
+
+    mysqlConnection.query(
+      "UPDATE rainfall_prediction SET model_accuracy = ? WHERE model_id = 1",
+      [arimaRaiFallAccuracy.data],
+      () => {}
+    );
+    console.log("update accuracy");
+  } catch (error) {
+    console.log(error);
+  }
   console.log("running a task every minute");
 });
 
--- WebApplication/Back-End/app/routes/accuracy.js
+++ WebApplication/Back-End/app/routes/accuracy.js
@@ -4,26 +4,25 @@ const axios = require("axios");
 const Router = express.Router();
 
 //get all weather details
-Router.get("/accuracy", async (req, res) => {
+Router.get("/arima_accuracy", async (req, res) => {
   try {
     const arimaTempAccuracy = await axios.get(
-      "https://agrobuddy-prediction.appspot.com/accuracy/arima/temp"
+      "https://agrobuddy-prediction.appspot.com/accuracy?model=arima&type=temp"
     );
     const arimaRaiFallAccuracy = await axios.get(
-      "https://agrobuddy-prediction.appspot.com/accuracy/arima/rain"
+      "https://agrobuddy-prediction.appspot.com/accuracy?model=arima&type=rainfall"
     );
     const arimaMarketPriceAccuracy = await axios.get(
-      "https://agrobuddy-prediction.appspot.com/accuracy/arima/market"
+      "https://agrobuddy-prediction.appspot.com/accuracy?model=arima&type=market"
     );
 
     res.send({
-        arima: {
-          temp: arimaTempAccuracy.data,
-          rain: arimaRaiFallAccuracy.data,
-          market: arimaMarketPriceAccuracy.data
-        }
+      arima: {
+        temp: arimaTempAccuracy.data,
+        rain: arimaRaiFallAccuracy.data,
+        market: arimaMarketPriceAccuracy.data
       }
-    );
+    });
   } catch (error) {
     console.log(error);
   }
