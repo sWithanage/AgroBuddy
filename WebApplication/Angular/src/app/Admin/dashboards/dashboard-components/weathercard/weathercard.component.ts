d6f0d3e9b018e96c26494986a420b034b1b80067
SaviniAthaudage <savini.athaudage@gmail.com>
2020-04-13 10:25:41 +0530

    update:admin dashboard current weather details

--- WebApplication/Angular/src/app/Admin/dashboards/dashboard-components/weathercard/weathercard.component.ts
+++ WebApplication/Angular/src/app/Admin/dashboards/dashboard-components/weathercard/weathercard.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthenticationService} from '../../../../authentication.service';
 import {DatePipe} from '@angular/common';
+import { AdminServiceService } from 'src/app/admin-service.service';
 @Component({
   selector: 'app-weather',
   templateUrl: './weathercard.component.html'
@@ -9,9 +9,16 @@ export class WeathercardComponent implements OnInit {
   weather: any[];
   topic;
 
-  constructor(private connectionService: AuthenticationService) { }
+  public currentWeather = [];
+
+  constructor(private _weatherService: AdminServiceService) { }
 
   ngOnInit() {
+
+    //using the service instance and fetching the weather data
+    this._weatherService.getCurrentWeather()
+      .subscribe(data => this.currentWeather = data);   //assigns the data recieved from the observable into local currentweathr property
+  
     // tslint:disable-next-line:comment-format
     //this.connectionService.getWeatherData().subscribe(
      // data => {
