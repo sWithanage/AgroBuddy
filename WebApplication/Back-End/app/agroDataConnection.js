84f0ae5790ac21b280af394fb890fe789d76271f
sachintha97 <ssamarasiri12@gmail.com>
2020-04-15 20:28:24 +0530

    fix : back-end connection error

--- WebApplication/Back-End/app/agroDataConnection.js
+++ WebApplication/Back-End/app/agroDataConnection.js
@@ -1,24 +1,61 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
+
+var db_config = {
+  host: "sql219.main-hosting.eu",
+  user: "u178617662_agroData",
+  password: "agroData",
+  database: "u178617662_agroData",
+};
+
+var connection;
+
+// recreate the connection, since the old one cannot be reused
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+
+  connection.connect(function (err) {
+    // the server is either down or restarting
+    if (err) {
+      console.log("error when connecting to db:", err);
+      // introduce a delay before attempting to reconnect
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log("AgroData database connection succeeded.");
+    }
+  });
+  connection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // connection to the MySQL server is usually lost due to either server restart
+      handleDisconnect();
+    } else {
+      // connection idle timeout
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+module.exports = connection;
 
 //creating connection configuration
-var connection = mysql.createConnection({
-    host : 'sql219.main-hosting.eu',
-    user : 'u178617662_agroData',
-    password : 'agroData',
-    database : 'u178617662_agroData',
-   // multipleStatements: true
-});
+// var connection = mysql.createConnection({
+//     host : 'sql219.main-hosting.eu',
+//     user : 'u178617662_agroData',
+//     password : 'agroData',
+//     database : 'u178617662_agroData',
+//    // multipleStatements: true
+// });
 
-//connect
-connection.connect((err) => {
-    if(err){
-        console.log('Database connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
-        //setTimeout(connection, 2000);
-        throw err;
-    }
-    else{
-        console.log('Weather database connection succeeded.');
-    }
-});
-
-module.exports = connection;
+// //connect
+// connection.connect((err) => {
+//     if(err){
+//         console.log('Database connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+//         //setTimeout(connection, 2000);
+//         throw err;
+//     }
+//     else{
+//         console.log('Weather database connection succeeded.');
+//     }
+// });
+//module.exports = connection;
--- WebApplication/Back-End/app/connection.js
+++ WebApplication/Back-End/app/connection.js
@@ -1,24 +1,63 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
+
+var db_config = {
+  host: "sql219.main-hosting.eu",
+  user: "u178617662_agrobackEnd",
+  password: "agrobackEnd",
+  database: "u178617662_agrobackEnd",
+};
+
+var connection;
+
+// recreate the connection, since the old one cannot be reused.
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+
+  connection.connect(function (err) {
+    // the server is either down or restarting
+    if (err) {
+      console.log("error when connecting to db:", err);
+      // introduce a delay before attempting to reconnect
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log("database connection succeeded.");
+    }
+  });
+  connection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // connection to the MySQL server is usually lost due to either server restart
+      handleDisconnect();
+    } else {
+      // connection idle timeout
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
+module.exports = connection;
 
 //creating connection configuration
-var mysqlConnection = mysql.createConnection({
-    host : 'sql219.main-hosting.eu',
-    user : 'u178617662_agrobackEnd',
-    password : 'agrobackEnd',
-    database : 'u178617662_agrobackEnd',
-   // multipleStatements: true
-});
+// var mysqlConnection = mysql.createConnection({
+//     host : 'sql219.main-hosting.eu',
+//     user : 'u178617662_agrobackEnd',
+//     password : 'agrobackEnd',
+//     database : 'u178617662_agrobackEnd',
+//    // multipleStatements: true
+// });
 
-//connect
-mysqlConnection.connect((err) => {
-    if(err){
-        console.log('Database connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
-        //setTimeout(mysqlConnection, 2000);
-        throw err;
-    }
-    else{
-        console.log('Database connection succeeded.');
-    }
-});
+// //connect
+// mysqlConnection.connect((err) => {
+//     if(err){
+//         console.log('Database connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+//         //setTimeout(mysqlConnection, 2000);
+//         throw err;
+//     }
+//     else{
+//         console.log('Database connection succeeded.');
+//     }
+// });
 
-module.exports = mysqlConnection;
+// module.exports = mysqlConnection;
