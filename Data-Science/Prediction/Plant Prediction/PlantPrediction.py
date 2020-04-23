bc922e18f6c29c828ae3bcdaabdd35517bbc7e92
Venura Pallawela <venura.pallawela213@gmail.com>
2020-04-23 22:03:00 +0530

    First commit to Plant Prediction

---
+++ Data-Science/Prediction/Plant Prediction/PlantPrediction.py
@@ -0,0 +1,5 @@
+import Calculation.TemperatureCalculation as calc
+
+
+tempDataArray = [22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5]
+values = calc.tempPrediction(tempDataArray)
