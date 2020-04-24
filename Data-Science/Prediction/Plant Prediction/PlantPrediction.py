6c8816f1f4fb7244ef4beaacca3087eaf72e5c73
Sasanka Withanage <awsnbwithanage@gmail.com>
2020-04-25 00:39:28 +0530

    File structure Fixed

--- Data-Science/Prediction/Plant Prediction/PlantPrediction.py
+++
@@ -1,5 +0,0 @@
-import Calculation.TemperatureCalculation as calc
-
-
-tempDataArray = [22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5, 22.5, 23.5]
-values = calc.tempPrediction(tempDataArray)
