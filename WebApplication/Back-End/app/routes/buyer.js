a716c87c4b2c9fcb7af0d07acb589948ebced135
sachintha97 <ssamarasiri12@gmail.com>
2020-04-27 06:45:24 +0530

    update: backend project

--- WebApplication/Back-End/app/routes/buyer.js
+++ WebApplication/Back-End/app/routes/buyer.js
@@ -4,43 +4,29 @@ const mysqlConnection = require("../connection");
 
 //get all buyer 
 router.get("/buyers", (req, res) => {
-    mysqlConnection.query("SELECT * from buyers", 
-    (err, rows, fields) => {
-        if(!err){
-            res.send(rows);
-        }
-        else{
-            console.log(err);
-        }
-    })
+    mysqlConnection.query("SELECT * from buyers",
+        (err, rows, fields) => {
+            if(!err){
+                res.send(rows);
+            }
+            else{
+                console.log(err);
+            }
+        })
 });
 
 //get a selected buyer details
 router.get("/buyers/:id", (req, res) => {
-    mysqlConnection.query("SELECT * from buyers WHERE buyerId=?", 
-    [req.params.id], 
-    (err, rows, fields) => {
-        if(!err){
-            res.send(rows);
-        }
-        else{
-            console.log(err);
-        }
-    })
-});
-
-//delete a buyer
-router.delete("/buyers", async (req, res) => {
-    mysqlConnection.query("DELETE FROM buyers WHERE buyerId=?", 
-    [req.body.cropId], 
-    (err, rows, fields) => {
-        if(!err){
-            res.send("Crop deleted successfully");
-        }
-        else{
-            console.log(err);
-        }
-    })
+    mysqlConnection.query("SELECT * from buyers WHERE buyerId=?",
+        [req.params.id],
+        (err, rows, fields) => {
+            if(!err){
+                res.send(rows);
+            }
+            else{
+                console.log(err);
+            }
+        })
 });
 
 //inserting a buyer
@@ -50,31 +36,50 @@ router.post("/buyers", async (req, res) => {
         buyerAddress: req.body.buyer_Address,
         buyerContactNumber: req.body.buyer_ContactNumber
     };
-    mysqlConnection.query("INSERT INTO buyers SET ?", 
-    data, 
-    (err, rows) => {
-      if (!err) {
-        res.send(true);
-      } else {
-        console.error(err);
-        res.send(err);
-      }
-    });
+    mysqlConnection.query("INSERT INTO buyers SET ?",
+        data,
+        (err, rows) => {
+            if (!err) {
+                res.send(true);
+            } else {
+                console.error(err);
+                res.send(err);
+            }
+        });
 });
+
+
+// delete selected user details
+router.post("/deleteBuyer/:id", async (req, res) => {
+    console.log(req.params.id);
+    mysqlConnection.query(
+        "DELETE FROM buyers WHERE buyerId ='"+req.params.id+"'",
+        (err, rows, fields) => {
+            if (!err) {
+                res.send(true);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+});
+
+
 
 //update crop details
 router.put("/buyers", async (req, res) => {
+    console.log(req);
     mysqlConnection.query(
         "UPDATE buyers SET buyerName='" + req.body.buyerName + "', " +
         "buyerAddress='"+req.body.buyerAddress+"'," +
         "buyerContactNumber='"+req.body.buyerContactNumber+"'" +
         "WHERE buyerId= '"+req.body.buyerId+"'",
         (err, rows) => {
-          if (!err) {
-            res.send(true);
-          } else {
-            res.send(err);
-          }
+            if (!err) {
+                res.send(true);
+            } else {
+                res.send(err);
+            }
         }
     );
 });
